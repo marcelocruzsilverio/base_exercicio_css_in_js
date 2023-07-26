b4fbe7b6f03b71e5c3c1ce4f0447a287a14f8536
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2023-07-26 20:38:00 -0300

    finalizado a lista de vagas com styled components

--- src/components/FormVagas/styles.ts
+++ src/components/FormVagas/styles.ts
@@ -7,6 +7,10 @@ export const Form = styled.form`
   padding: 32px;
   border-radius: 12px;
   margin-top: 40px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
 `
 
 export const BtnSearch = styled.button`
@@ -18,9 +22,18 @@ export const BtnSearch = styled.button`
   color: var(--cor-secundaria);
   margin-left: 8px;
   cursor: pointer;
+
+  @media (max-width: 768px) {
+    margin-left: 0;
+  }
 `
 
 export const Input = styled.input`
   padding: 0 16px;
   outline-color: var(--cor-principal);
+
+  @media (max-width: 768px) {
+    padding: 8px;
+    margin-bottom: 8px;
+  }
 `

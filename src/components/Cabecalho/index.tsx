935882867f3cc8ddae97c9df894642eb227000c2
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2023-07-26 19:30:51 -0300

    finalizado o header com styled components

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,9 @@
-import styles from './Cabecalho.module.css'
+import { HeaderStyle } from './styles'
 
 const Cabecalho = () => (
-  <header className={styles.cabecalho}>
+  <HeaderStyle>
     <h1>EBAC Jobs</h1>
-  </header>
+  </HeaderStyle>
 )
 
 export default Cabecalho
